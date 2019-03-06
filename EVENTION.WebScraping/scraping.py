d38e1384ba711be8b4d91cd5405bca414191c950
sleter <sleter97@gmail.com>
2019-03-06 21:03:45 +0100

    scrap

--- EVENTION.WebScraping/scraping.py
+++ EVENTION.WebScraping/scraping.py
@@ -1,10 +1,30 @@
 import bs4 as bs
-import urllib.request
+import urllib.request, sys
+from PyQt5.QtCore import QUrl
+from PyQt5.QtWeb
+from PyQt5.QtWidgets import QApplication
+
+
+class Client(QWebPage):
+    def __init__(self, url):
+        self.app = QApplication(sys.argv)
+        QWebPage.__init__(self)
+        self.loadFinished.connect(selff.on_page_load)
+        self.mainFrame().load(QUrl(url))
+        self.app.exec_()
+    def on_page_load(self):
+        self.app.quit()
 
 class Scrap:
     def __init__(self):
-        pass #TODO read config
-
-    def scrap_website_name(self):
         pass
 
+    def scrap_kiwiportal(self, url):
+        client_response = Client(url)
+        source = client_response.mainFrame().innerHtml()
+        soup = bs.BeautifulSoup(source, 'lxml')
+        simple_strong = soup.find('strong')
+        print(simple_strong)
+
+s = Scrap()
+s.scrap_kiwiportal('https://www.kiwiportal.pl/wydarzenia/m/warszawa')
