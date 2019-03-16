2a1a0d7700c8537e193808a1ffda74f71583cc89
sleter <sleter97@gmail.com>
2019-03-16 12:34:59 +0100

    major parsing changes

--- EVENTION.DataHarvester/EVENTION.WebScraping/scraping.py
+++ EVENTION.DataHarvester/EVENTION.WebScraping/scraping.py
@@ -1,5 +1,10 @@
 import bs4 as bs
-import urllib.request, sys, json, datetime
+from geojson import Point
+from pyjsparser import PyJsParser
+from opencage.geocoder import OpenCageGeocode
+import urllib.request, sys, json, datetime, re, pprint
+
+API_KEY = '908e5c8c9d9a42ad9d67ef77cf199358'
 
 class Scrap:
     def __init__(self):
@@ -7,44 +12,49 @@ class Scrap:
             config = json.load(file)
         self.event_labels = config["event_labels"]
         self.event_address_labels = config["event_address_labels"]
+        self.geocoder = OpenCageGeocode(API_KEY)
+        self.p = PyJsParser()
 
-        self.month_code = {
-            'STY': 1,
-            'LUT': 2,
-            'MAR': 3,
-            'KWI': 4,
-            'MAJ': 5,
-            'CZE': 6,
-            'LIP': 7,
-            'SIE': 8,
-            'WRZ': 9,
-            'PAZ':10,
-            'LIS': 11,
-            'GRU': 12
-        }
+    def parse_date(self, date):
+        try:
+            day, month, year = date[0]['value'].split('.')
+            hour, minute = date[2]['value'].split(':')
+        except AttributeError:
+            return False
+        return datetime.datetime(int(year), int(month), int(day), int(hour), int(minute), 0)
 
-    def parse_date(self, event):
-        day = int(event.find('span', attrs={'class': 'dateDay'}).getText())
-        month = self.month_code[event.find('span', attrs={'class': 'dateMonth'}).getText()]
-        year = datetime.datetime.now().year
-        hour, minutes = event.find('span', attrs={'class': 'dateTimeOrYear'}).getText().split(':')
-        seconds = 0
-        return datetime.datetime(year,month,day,int(hour),int(minutes),seconds)
+    def create_geojson(self, query):
+        results = self.geocoder.geocode(query)
+        return Point((results[0]['geometry']['lng'], results[0]['geometry']['lat']))
 
     def event_parser(self, event):
         proper_event = {}
-        name = event.find('strong').getText()
-        #TODO description
-        shortDescription = event.find('script', attrs={'class': 'application/ld+json'})
-        longDescription = ""
+
+        source = urllib.request.urlopen(event)
+        soup = bs.BeautifulSoup(source, 'lxml')
+        info = soup.find_all('script')[8].getText()
+        info_dict = self.p.parse(info)
+
+        name = info_dict['body'][4]['declarations'][0]['init']['properties'][2]['value']['value']
+        url = info_dict['body'][4]['declarations'][0]['init']['properties'][5]['value']['value']
+        longDescription = info_dict['body'][4]['declarations'][0]['init']['properties'][6]['value']['value']
+        longDescription = bs.BeautifulSoup(longDescription, 'lxml')
+        longDescription = ''.join(longDescription.find_all(text=True))
+        shortDescription = ' '.join(re.split(r'(?<=[.:;])\s', longDescription)[:2]) + ' [...] '
         creationDate = datetime.datetime.now()
-        eventStart = self.parse_date(event)
-        eventEnd = eventStart
-        # owner = 
-        # geoJSON = 
+        eventStart = self.parse_date(info_dict['body'][4]['declarations'][0]['init']['properties'][16]['value']['elements'])
+        if self.parse_date(info_dict['body'][4]['declarations'][0]['init']['properties'][17]['value']['elements']):
+            eventEnd = self.parse_date(info_dict['body'][4]['declarations'][0]['init']['properties'][17]['value']['elements'])
+        else:
+            eventEnd = eventStart
+        owner = "EVENTION"
+        categories_elements = info_dict['body'][4]['declarations'][0]['init']['properties'][18]['value']['elements']
+        categories = []
+        for category in categories_elements:
+            categories.append(category['value'].lower())
         # address = 
+        # geoJSON = self.create_geojson()
         # imageSource = 
-        # category = 
         # # --------------------------------------------------------------------
         # country = 
         # city = 
@@ -67,14 +77,16 @@ class Scrap:
         except:
             print('Website ERROR')
         soup = bs.BeautifulSoup(source, 'lxml')
-        event_list = soup.find_all('li', attrs={'class': 'ui-datascroller-item'} )
-        content_list = []
-        for event in event_list:
-            try:
-                content_list.append(self.event_parser(event))
-            except AttributeError:
-                pass
-        print(len(content_list))
+        event_list = soup.find_all('a', {'href': re.compile(r'https:\/\/www\.kiwiportal\.pl\/wydarzenia/[0-9]+.*')})
+        event_list = list(set([event['href'] for event in event_list]))
+        self.event_parser(event_list[0])
+        # for event in event_list:
+        #     try:
+        #         self.event_parser(event)
+        #     except AttributeError:
+        #         pass
+        # print(len(event_list))
 
 s = Scrap()
 s.scrap_kiwiportal('https://www.kiwiportal.pl/wydarzenia/m/warszawa')
+# s.create_geojson('Polska, Poznań, ulica Stróżyńskiego 17c/10')
