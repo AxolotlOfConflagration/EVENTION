17ee41065b1b201302cd564d456396bedf448a52
m-wlodarczyk <mich.j.wlodarczyk@gmail.com>
2019-04-01 10:34:00 +0200

    improved EventDrawer and created EventsMap

--- EVENTION.WebClient/src/App.js
+++ EVENTION.WebClient/src/App.js
@@ -1,14 +1,21 @@
 import React, { Component } from "react";
 import "antd/dist/antd.css";
+import { Col } from "antd";
 import MyLayout from "./containers/Layout";
 import ShortEventList from "./containers/ShortEventList";
+import EventMap from "./components/EventsMap";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <MyLayout>
-          <ShortEventList />
+          <Col span={14}>
+            <ShortEventList />
+          </Col>
+          <Col span={10}>
+            <EventMap />
+          </Col>
         </MyLayout>
       </div>
     );
--- EVENTION.WebClient/src/components/EventsMap.js
+++ EVENTION.WebClient/src/components/EventsMap.js
@@ -1,8 +1,27 @@
 import React from "react";
+import L from "leaflet";
+import "leaflet/dist/leaflet.css";
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+  width: ${props => props.width}
+  height: ${props => props.height}
+`;
 
 class EventMap extends React.Component {
+  componentDidMount() {
+    this.map = L.map("map", {
+      center: [52.406, 16.925],
+      zoom: 13
+    });
+
+    L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+      maxZoom: 20
+    }).addTo(this.map);
+  }
+
   render() {
-    return <p>TO DO</p>;
+    return <Wrapper width="100%" height="800px" id="map" />;
   }
 }
 
