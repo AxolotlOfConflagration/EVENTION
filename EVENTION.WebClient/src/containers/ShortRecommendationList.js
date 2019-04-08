9c44a6e314bd4486b2ea6f6dcd2907f57d3a3a72
Agulaa <agnieszkaxrusin@gmail.com>
2019-04-08 17:12:23 +0200

    recommendation AGa

--- EVENTION.WebClient/src/containers/ShortRecommendationList.js
+++ EVENTION.WebClient/src/containers/ShortRecommendationList.js
@@ -18,19 +18,15 @@ class ShortRecommendationList extends React.Component {
         });
         console.log(res.data);
         console.log(this.state.EventId.recommendation);
-
-        this.state.EventId.recommendation.forEach(rec => {
-          axios.get(
-            "http://localhost:9000/event".concat(rec).then(result => {
-              ShortEvent.push(result);
-              console.log(ShortEvent);
-            })
-          );
-        });
       });
-    // this.state.EventId.recommendation.forEach(rec => {
-    //   console.log(rec);
-    // });
+    this.state.EventId.recommendation.forEach(rec => {
+      axios.get("http://localhost:9000/event".concat(rec)).then(result => {
+        console.log(result);
+        this.state.ShortEvents.push(result);
+
+        console.log(ShortEvent);
+      });
+    });
   }
 
   render() {
