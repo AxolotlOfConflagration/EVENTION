40b23f0fbdcebca9ef3572e34f5ba24dc2b8e09d
Bartosz Mikulski <bartmiki97@gmail.com>
2019-04-24 22:30:04 +0200

    Added Followers Controller and routes

---
+++ EVENTION.App/app/controllers/FollowerController.scala
@@ -0,0 +1,27 @@
+package controllers
+
+import javax.inject.Inject
+import models.database.Follower
+import play.api.libs.json.Json
+import play.api.mvc.{AbstractController, Action, AnyContent, ControllerComponents}
+import repositories.{FollowersRepository, UserRepository}
+
+import scala.concurrent.ExecutionContext
+import scala.util.Success
+
+class FollowerController @Inject()
+(repo: FollowersRepository, cc: ControllerComponents)
+(implicit ec: ExecutionContext) extends AbstractController(cc) {
+
+  def follow(userId: Long, whoId: Long): Action[AnyContent] = Action.async {
+    repo.follow(Follower(Some(userId), whoId)).map(_ => Ok)
+  }
+
+  def unfollow(userId: Long, whoId: Long): Action[AnyContent] = Action.async {
+    repo.unfollow(userId, whoId).map(_ => Ok)
+  }
+
+  def following(userId: Long): Action[AnyContent] = Action.async {
+    repo.following(userId).map(result => Ok(Json.toJson(result)))
+  }
+}
