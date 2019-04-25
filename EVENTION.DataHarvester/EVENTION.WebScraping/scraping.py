701d2d53c2311c6c9d6f6c25acdd2f04e1eb5d0c
sleter <sleter97@gmail.com>
2019-04-25 15:35:01 +0200

    html issue

--- EVENTION.DataHarvester/EVENTION.WebScraping/scraping.py
+++ EVENTION.DataHarvester/EVENTION.WebScraping/scraping.py
@@ -47,7 +47,8 @@ class Scrap:
         url = info_dict['body'][4]['declarations'][0]['init']['properties'][5]['value']['value']
         longDescription = info_dict['body'][4]['declarations'][0]['init']['properties'][6]['value']['value']
         longDescription = bs.BeautifulSoup(longDescription, 'lxml')
-        longDescription = ''.join(longDescription.find_all(text=True))
+        longDescription = str(longDescription)
+        # longDescription = ''.join(longDescription.find_all(text=True))
         shortDescription = ' '.join(re.split(r'(?<=[.:;])\s', longDescription)[:2]) + ' [...] '
         tags = info_dict['body'][4]['declarations'][0]['init']['properties'][37]['value']['value']
         creationDate = datetime.datetime.now()
