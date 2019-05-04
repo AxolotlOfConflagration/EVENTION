bcd56754206e964a6b4f45b0ede2987ab996cbe8
Bartosz Mikulski <bartosz.mikulski@deep.bi>
2019-05-04 14:21:24 +0200

    User authentication

--- EVENTION.App/app/controllers/AuthController.scala
+++ EVENTION.App/app/controllers/AuthController.scala
@@ -1,22 +1,37 @@
 package controllers
 
 import javax.inject.Inject
+import models.database.User
 import org.pac4j.core.profile.CommonProfile
+import org.pac4j.oauth.profile.google2.Google2Profile
 import org.pac4j.play.scala.{Security, SecurityComponents}
 import play.api.libs.json.Json
-import play.api.mvc.{AbstractController, Action, AnyContent, ControllerComponents}
-import repositories.BusinessRepository
+import play.api.mvc.{Action, AnyContent}
+import repositories.UserRepository
 
 import scala.concurrent.ExecutionContext
 
 class AuthController @Inject()
 (
-  repo: BusinessRepository,
+  repo: UserRepository,
   override val controllerComponents: SecurityComponents
 )(implicit ec: ExecutionContext) extends Security[CommonProfile] {
 
   def loginWithGoogle: Action[AnyContent] = Secure("Google2Client") { implicit request =>
-    Ok(Json.toJson(request.profiles.map(usr => s"${usr.getEmail} ${usr.getUsername} ${usr.getDisplayName}")))
+    val profile = request.profiles.head
+    val fullname = profile.getDisplayName.split("//s+")
+
+    val user = User(
+      id = None,
+      firstName = if(fullname.length > 1) fullname(0) else "",
+      lastName = if(fullname.length > 2) fullname(1) else "",
+      nick = Some(profile.getDisplayName),
+      externalId = Some(profile.getEmail)
+    )
+
+    val userId = repo.registerOrLogin(user)
+
+    Redirect(s"http://localhost:3000/login?userid=${userId}")
   }
 
   def callback: Action[AnyContent] = Action { implicit request =>
--- EVENTION.WebClient/src/Login.js
+++ EVENTION.WebClient/src/Login.js
@@ -1,14 +1,17 @@
 import React from "react";
 import { Button } from "antd";
 import TestLayout from "./containers/TestLayout";
-import axios from "axios";
+// import axios from "axios";
 
 class Login extends React.Component {
   render() {
+    document.cookie = "USER_ID="+window.location.search+"; path=/"
     return (
       <div className="Login">
         <TestLayout>
-          <Button onClick={() => {}}>LOGIN</Button>
+          <b>{window.location.search}</b>
+          <div></div>
+          <Button href="http://localhost:9000/login">LOGIN</Button>
         </TestLayout>
       </div>
     );
