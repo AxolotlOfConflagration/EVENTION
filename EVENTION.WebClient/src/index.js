b647c3b12d8e9fd712b73acb474243dd59b75fcc
Agulaa <agnieszkaxrusin@gmail.com>
2019-05-06 13:00:46 +0200

    change

--- EVENTION.WebClient/src/index.js
+++ EVENTION.WebClient/src/index.js
@@ -3,15 +3,7 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import { Route, BrowserRouter as Router } from "react-router-dom";
 import Home from "./Home";
-<<<<<<< HEAD
 //import TestHome from "./TestHome";
-=======
-
-import Activate from "./Activate";
-
-//import TestHome from "./TestHome";
-
->>>>>>> 8aaa603af17bf63b1c1cbd144104c668b8487a3e
 import AddEvent from "./AddEvent";
 import Recommendation from "./Recommendation";
 import Login from "./Login";
@@ -23,15 +15,8 @@ const routing = (
       <Route exact path="/" component={Home} />
       <Route path="/DodajWydarzenie" component={AddEvent} />
       <Route path="/Rekomendacje" component={Recommendation} />
-<<<<<<< HEAD
       <Route path="/Login" component={Login} />
       <Route path="/OstatniaAktywnosc" component={Activate} />
-=======
-
-      <Route path="/OstatniaAktywnosc" component={Activate} />
-
-
->>>>>>> 8aaa603af17bf63b1c1cbd144104c668b8487a3e
     </div>
   </Router>
 );
