43a8efd7349932db60196f6a1dd7d14924164cb9
Bartosz Mikulski <bartosz.mikulski@deep.bi>
2019-05-26 15:48:27 +0200

    Added startup recommendations

--- EVENTION.App/app/controllers/AuthController.scala
+++ EVENTION.App/app/controllers/AuthController.scala
@@ -1,20 +1,25 @@
 package controllers
 
 import javax.inject.Inject
-import models.database.User
+import models.database.{Recommendation, User}
 import org.pac4j.core.profile.CommonProfile
 import org.pac4j.oauth.profile.google2.Google2Profile
 import org.pac4j.play.scala.{Security, SecurityComponents}
 import org.pac4j.play.store.PlaySessionStore
 import play.api.libs.json.Json
 import play.api.mvc.{Action, AnyContent}
-import repositories.UserRepository
+import repositories.{EventRepository, RecommendationRepository, UserRepository}
 
+import concurrent.Await
 import scala.concurrent.ExecutionContext
+import scala.concurrent.duration.Duration
+import scala.util.Random
 
 class AuthController @Inject()
 (
   repo: UserRepository,
+  recommendationRepo: RecommendationRepository,
+  eventRepo: EventRepository,
   override val controllerComponents: SecurityComponents
 )(implicit ec: ExecutionContext) extends Security[CommonProfile] {
 
@@ -22,25 +27,25 @@ class AuthController @Inject()
     val profile = request.profiles.head
     val fullname = profile.getDisplayName.split("//s+")
 
-//    println(request.cookies.map(c => s"${c.name} ${c.value}").mkString("\n"))
-
     val user = User(
       id = None,
-      firstName = if(fullname.length > 1) fullname(0) else "",
-      lastName = if(fullname.length > 2) fullname(1) else "",
+      firstName = if (fullname.length > 1) fullname(0) else "",
+      lastName = if (fullname.length > 2) fullname(1) else "",
       nick = Some(profile.getDisplayName),
       externalId = Some(profile.getEmail)
     )
 
     val userId = repo.registerOrLogin(user)
+    val ids = Await.result[Seq[Long]](eventRepo.all().map(_.map(_.id.getOrElse(-1L))), Duration.Inf)
+    val randoms = Random.shuffle(ids.filterNot(_ == -1L)).take(5).mkString("[", ",", "]")
+
+    recommendationRepo.addOrReplaceRecommendation(Recommendation(userId, randoms))
 
     Redirect(s"http://localhost:3000/login?userid=$userId")
-    //Redirect(s"http://localhost:3000")
   }
 
   def loginBasic: Action[AnyContent] = Secure("DirectBasicAuthClient ") { implicit request =>
     val userId = 1
-//    Redirect(s"http://localhost:3000/login?userid=$userId&type=business")
     Ok("""{"userId": 1, "type": "business"}""")
   }
 
